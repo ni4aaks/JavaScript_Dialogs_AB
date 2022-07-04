7331a7c00b3b7582fd95a5a171b4fe9ffc602627
ni4aaks <maris.nitcis@gmail.com>
2022-07-04 15:30:36 +0300

    Changes

--- Class_13_20220629/js/script.js
+++ Class_13_20220629/js/script.js
@@ -33,5 +33,15 @@ for (let i = 1; i < 11; i++) {
     if (i % 2 > 0)
         text3 = text3 + i + "<br>";
 }
-console.log(text2);
+console.log(text3);
 document.getElementById("for_automatic_count_even_until_10").innerHTML = text3;
+
+let text4 = "";
+for (let i = 1; i < 11; i++) {
+    if (i % 2 > 0)
+        text4 = text4 + i + "Pāra skaitļi <br>";
+    else
+        text4 = text4 + i + "Nepāra skaitļi <br>";
+}
+console.log(text4);
+document.getElementById("for_automatic_count_odd_until_10_with_even_descripttion").innerHTML = text4;
