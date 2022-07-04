58a62fc9e88063a277cbf02777b7a1c0846e1566
ni4aaks <maris.nitcis@gmail.com>
2022-07-04 13:41:39 +0300

    changes

--- Class_13_20220629/js/script.js
+++ Class_13_20220629/js/script.js
@@ -1,15 +1,29 @@
 console.log("Skripta izpildes sākums:\n" + new Date());
 document.getElementById("manual_count").innerHTML = "1<br>2<br>3<br>4<br>";
 
-const cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
+//const cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
+
+// for (  ;  ;  ){}
+// for (sā - darbības ciklu uzsākot ; n - nosacījumms cikla turpināšanai ; so - soļu veikšana) {}
+// sā -> n 9nosacījums ir spēkā) -> so -> n(-"-") -> so -> n (nav spēkā)
+// pārējām pie nākās darbības aiz cikla
 
 let text = "";
-for (let i = 0; i < cars.length; i++) {
-    text += cars[i] + "<br>";
+for (let i = 1; i < 5; i++) {
+
+    text = text + i + "<br>";
 }
 console.log(text);
-document.getElementById("automatic_count").innerHTML = text;
+document.getElementById("for_automatic_count").innerHTML = text;
 /*
 text += cars[0] + "<br>";
 text = text + cars[0] + "<br>"
 */
+
+let text2 = "";
+for (let i = 1; i < 11; i++) {
+    if (i % 2 == 0)
+        text2 = text2 + i + "<br>";
+}
+console.log(text2);
+document.getElementById("for_automatic_count_odd_until_10").innerHTML = text2;
