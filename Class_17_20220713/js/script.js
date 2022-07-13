a9178f3129b63feb31bfb9db8885d5eb0deca8a7
ni4aaks <maris.nitcis@gmail.com>
2022-07-13 20:20:51 +0300

    Changes

--- Class_17_20220713/js/script.js
+++ Class_17_20220713/js/script.js
@@ -21,12 +21,29 @@ person.fullName
     }
 person.fullName();
 'John Doe'
+/*
+//salīdzināt ievadīto tekstu ar objektā iekļauto masīvu
 */
 
-const EU = {
-    countrys: ['Latvija', 'Lietuva', 'Cehija'],
+let prompt_value = prompt("Enter country name:");
 
+function EU_country_find(value, index, array) {
+    console.log(prompt_value);
+    console.log(value);
+    return value == prompt_value;
+}
+
+const EU = {
+    countries: ["Latvija", "Lietuva", "Cehija"],
+    // prompt_value: prompt("Enter country name:"),
+    /*  EU_country_find: function (value, index, array) {
+        // console.log(this.prompt_value);
+        console.log(prompt_value);
+        console.log(value);
+        // return value == this.prompt_value;},
+        return value == prompt_value;},*/
     EU_classification: function () {
-        return this.countrys == prompt("Enter country name") ? "EU" : "Not EU"
-    }
+        // return this.countries.find(this.EU_country_find) == undefined ? "Not EU" : "EU";
+        return this.countries.find(EU_country_find) == undefined ? "Not EU" : "EU";
+    },
 };
