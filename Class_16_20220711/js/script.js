559b3131fe03397de494797c187aed9b8fff27fd
ni4aaks <maris.nitcis@gmail.com>
2022-07-13 14:31:39 +0300

    Changes

--- Class_16_20220711/js/script.js
+++ Class_16_20220711/js/script.js
@@ -133,3 +133,15 @@ function convertDDToDMS(deg, lng){
         sec : s
     };
 }
+
+function coordToStrWithDecimals(coord)
+{   
+    let centiSecs = Math.round(Math.abs(coord) * 360000)
+    let frac = Math.floor(centiSecs % 100)
+    let seconds = Math.floor(centiSecs / 100)
+    let sec = Math.floor(seconds % 60)
+    let minutes = Math.floor(seconds / 60)
+    let min = minutes % 60
+    let deg = Math.floor(minutes / 60)
+    return deg + "°" + ((min < 10) ? "0" : "") + min + "'" + ((sec < 10) ? "0" : "") + sec + "." + ((frac < 10) ? "0" : "") + frac + '"'
+}
