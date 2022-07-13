626a67499df925fd112e3c2e405dec2ef4f46bb6
ni4aaks <maris.nitcis@gmail.com>
2022-07-13 13:39:45 +0300

    Chages

--- Class_16_20220711/js/script.js
+++ Class_16_20220711/js/script.js
@@ -53,7 +53,7 @@ const cube = map(cube_for_array_each_element, numbers);
 console.log(cube);
 */
 
-
+/*
 function dmsToDeg(dms) {
     if (!dms) {
         return Number.NaN;
@@ -106,4 +106,30 @@ function dms2deg(s) {
     }
 
     return result;
+}
+*/
+
+function convertDDToDMS(deg, lng){
+    var d = parseInt(deg);
+    var minfloat  = Math.abs((deg-d) * 60); 
+    var m = Math.floor(minfloat);
+    var secfloat = (minfloat-m)*60;
+    var s = Math.round(secfloat); 
+    d = Math.abs(d);
+
+    if (s==60) {
+        m++;
+        s=0;
+    }
+    if (m==60) {
+        d++;
+        m=0;
+    }
+
+    return {
+        dir : deg<0?lng?'W':'S':lng?'E':'N',
+        deg : d,
+        min : m,
+        sec : s
+    };
 }
