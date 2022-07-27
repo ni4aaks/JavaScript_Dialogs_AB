2a13cbc1b932ef713e7dda932ceb60452bff000e
ni4aaks <maris.nitcis@gmail.com>
2022-07-27 14:31:16 +0300

    changes

--- Class_19_20220720/js/script.js
+++ Class_19_20220720/js/script.js
@@ -1,33 +1,44 @@
-//Masīva elementu salīdzināšanas izmantojot funkcijas
-
 /*
-let grades = [91, 66, 77, 84, 66];
-let search = 66;
-
-function searchGrades(grades, search) {
-    for (var i = 0; i < grades.length; i++) {
-        if (grades[i] == search) {
-            console.log(`The grades ${search} exisit`);
-            break;
-        }
-    }
-}
-searchGrades(grades, search);
+var x1 = 10;
+document.getElementById("demo1").innerHTML = `x1=${x1}`;
+var x2 = x1;
+x2 = 20;
+document.getElementById("demo2").innerHTML = "x2="+x2;
+document.getElementById("demo3").innerHTML = `and now - x1=${x1}`;
 */
 
-// saīstinātā versijia 1. versija
+const obj1 = { property1: 10 };
+//document.getElementById("demo1").innerHTML = `obj1.property1=${obj1.property1}`;
 
-/*
-let grades = [91, 66, 77, 84, 66];
-let search = 66;
+const obj2 = obj1;
+obj2.property1 = 20;
+//document.getElementById("demo2").innerHTML = `obj2.property1=${obj2.property1}`;
+//document.getElementById("demo3").innerHTML = `and now - obj1.property1=${obj1.property1}`;
 
-function searchGrades(grades, search) {
-    for (currentGrade of grades) {
-        if (currentGrade == search) {
-            console.log(`The grades ${search} exisit`);
-            break;
-        }
-    }
+Object.defineProperty(obj2, "property2", {
+    value: 100,
+    enumerable: true,
+});
+
+Object.defineProperty(obj2, "property3", {
+    value: "Aaaa",
+    configurable: false,
+});
+
+let txt = "";
+for (let current_property in obj1) {
+    txt += current_property + ": " + obj1[current_property] + "<br>";
 }
-searchGrades(grades, search);
-*/
+
+for (let current_property in obj1) {
+    console.log(current_property);
+}
+
+//for (let current_property of obj1) {
+//  console.log(current_property);
+//}
+
+document.getElementById("demo1").innerHTML = txt;
+
+delete obj2.property3;
+
