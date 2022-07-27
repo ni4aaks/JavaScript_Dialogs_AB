30252820a3c8788cf6dc90be012e68d35803b334
ni4aaks <maris.nitcis@gmail.com>
2022-07-27 14:31:33 +0300

    changes

---
+++ Class_20_20220725/js/script1.js
@@ -0,0 +1,34 @@
+// https://www.w3schools.com/js/js_object_constructors.asp
+
+// Constructor function for Person objects
+function Person(first, last, age, eye) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eye;
+}
+
+// Create two Person objects
+const myFather = new Person("John", "Doe", 50, "blue");
+const myMother = new Person("Sally", "Rally", 48, "green");
+
+// Display age
+document.getElementById("demo1").innerHTML =
+    "My father is " + myFather.age + ". My mother is " + myMother.age + ".";
+
+console.log(myFather);
+
+myFather.nationality = "English";
+console.log(myFather);
+
+myFather.name = function () {
+    return this.firstName + " " + this.lastName;
+};
+console.log(myFather.name());
+
+Object.defineProperty(myFather, "get_nationality", {
+    get: function () {
+        return this.nationality;
+    },
+});
+console.log(myFather.get_nationality);
