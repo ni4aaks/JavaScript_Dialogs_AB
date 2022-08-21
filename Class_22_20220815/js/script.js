a3ec046057b448a7f2bcb46307d397bb2a631fb9
ni4aaks <maris.nitcis@gmail.com>
2022-08-21 14:18:12 +0300

    changes

---
+++ Class_22_20220815/js/script.js
@@ -0,0 +1,48 @@
+// location :
+console.log(window.location);
+console.log(document.location);
+console.log(location);
+
+// URL ("pina" adrese)
+console.log(location.href);
+
+// protokols (https un https - datu plūsma caur tīklu, bet varbūt arī lokālo aplikāciju darbināšana)
+console.log(location.protocol);
+
+// host (IP vai "vārdiskā" IP adrese)
+// ~ mājas darbs - palasīt par DNS
+console.log(location.host);
+
+//port 
+// ~ mājas darbs - palasīt par portiem un to pielietošanu (portu numuru diapapazondi)
+console.log(location.port);
+
+// web servisa "ceļš"
+// pirmā "/" aiz host'a - web servisa ceļu "saknes"
+console.log(location.pathname);
+
+// meklēšanas kritēriji 
+// aiz "?" zīmes, tie var būt vairāki - savā starpā atdalīti ar  "&" zīmi
+console.log(location.search);
+
+// meklēšanas kritēriji
+// aiz "?" zīmes, tie var būt vairāki - savā starpā atdalīti ar "&" zīmi
+
+// HTML dokumenta apakšsadaļas nosaukums
+console.log(location.hash);
+
+// lietotāja vārds un parole
+// pirms hostname pirms "@" zīmes
+// https://developer.mozilla.org/en-US/docs/Web/API/URL/username
+// https://developer.mozilla.org/en-US/docs/Web/API/URL/password
+// const url = new URL('https://anonymous:flabada@developer.mozilla.org/en-US/docs/Web/API/URL/username');
+// console.log(url.username) // Logs "anonymous"
+// console.log(url.password) // Logs "flabada"
+
+//alert('Tulīt ielādēsies cita lapa');
+
+// assign(), replace(), reload()
+// svarīgi vai ir vai nav ieraksts iekš History - attiecīgi iespēja pārviettotes ar Back/Next pogām
+//         vai notiek lapas satura izgūšana no servera vai no pārluka Cache
+//location.replace('http://127.0.0.1:5500/Dialogs_AB/Class_21/index.html?q1=1&q2=a');
+
